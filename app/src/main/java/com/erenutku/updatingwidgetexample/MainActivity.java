29c11b7faae6c2ee058e38ff3cca9be0a399e241
Zoff <zoff99@users.noreply.github.com>
2018-07-27 14:54:26 +0200

    add manually update feature

--- app/src/main/java/com/erenutku/updatingwidgetexample/MainActivity.java
+++ app/src/main/java/com/erenutku/updatingwidgetexample/MainActivity.java
@@ -1,25 +1,29 @@
 package com.erenutku.updatingwidgetexample;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import static com.erenutku.updatingwidgetexample.UpdateService.update_status_on_widget;
 
 public class MainActivity extends AppCompatActivity
 {
     int appWidgetId;
+    static TextView log_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        log_text = (TextView) findViewById(R.id.log_text);
 
         try
         {
@@ -45,12 +49,61 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
 
-        // finish();
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
+
+        // Update the widget on first start -------------
+        final Context c = this;
+        final Thread th = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    final long start_timestamp = System.currentTimeMillis();
+                    runOnUiThread(new Runnable()
+                    {
+                        public void run()
+                        {
+                            try
+                            {
+                                log_text.setText("updating widget status ...");
+                            }
+                            catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    update_status_on_widget(c);
+                    runOnUiThread(new Runnable()
+                    {
+                        public void run()
+                        {
+                            try
+                            {
+                                final long end_timestamp = System.currentTimeMillis();
+                                log_text.setText("READY (took " + (long) ((end_timestamp - start_timestamp) / 1000) +
+                                                 " seconds)");
+                            }
+                            catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+        th.start();
+        // Update the widget on first start -------------
     }
 }
